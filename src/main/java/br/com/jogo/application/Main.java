80750addc84dcb1bbdd5d923d3d105f71db83d81
Ana Cecilia <anaceiliahope@gmail.com>
2023-12-14 12:52:41 -0300

    setup projet

---
+++ src/main/java/br/com/jogo/application/Main.java
@@ -0,0 +1,11 @@
+package br.com.jogo.application;
+
+import br.com.jogo.boardgame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+
+    }
+}
