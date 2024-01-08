032243738f31b423f870ccee73a255b1207b0f4c
Ana Cecilia <anaceiliahope@gmail.com>
2024-01-08 11:17:46 -0300

    setup projet

---
+++ src/main/java/br/com/jogo/boardgame/BoardException.java
@@ -0,0 +1,7 @@
+package br.com.jogo.boardgame;
+
+public class BoardException extends RuntimeException{
+    public BoardException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/br/com/jogo/chess/ChessMatchException.java
@@ -0,0 +1,7 @@
+package br.com.jogo.chess;
+
+public class ChessMatchException extends RuntimeException{
+    public ChessMatchException(String msg) {
+        super(msg);
+    }
+}
