70baefa645f8fe1f174ce9acd1522464c54785c9
Ana Cecilia <anaceiliahope@gmail.com>
2024-01-09 10:44:37 -0300

    setup projet

--- src/main/java/br/com/jogo/application/Program.java
+++ src/main/java/br/com/jogo/application/Program.java
@@ -1,9 +1,8 @@
 package br.com.jogo.application;
 
-import br.com.jogo.boardgame.Board;
 import br.com.jogo.chess.ChessMatch;
 
-public class Main {
+public class Program {
     public static void main(String[] args) {
         ChessMatch chessMatch = new ChessMatch();
         UI.printBoard(chessMatch.getPieces());
